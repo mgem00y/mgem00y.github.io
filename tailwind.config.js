ddb3075b6556c4d8daba4acb1f36b25bdd9d29db
Brian Yang <brian@aerodroid.com>
2024-02-11 19:19:28 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["bebas-neue", "ui-sans-serif", "system-ui"],
+    },
+  },
+  plugins: [],
+  // corePlugins: {
+  //   preflight: false,
+  // },
+};
